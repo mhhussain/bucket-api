f6f3133a38e6f945996083c459abf9f1154b4906
moohh <mo051191@yahoo.com>
2019-12-23 14:42:45 -0500

    defaults to fs adapter

---
+++ src/fsAdapter.js
@@ -0,0 +1,19 @@
+
+let configs = require('../configs');
+
+let createFile = (bucket, id, data) => {
+    return new Promise((resolve, reject) => {
+        reject('Not implemented');
+    });
+};
+
+let retrieveFile = (bucket, id) => {
+    return new Promise((resolve, reject) => {
+        reject('Not implemented');
+    });
+};
+
+module.exports = {
+    createFile,
+    retrieveFile
+};
